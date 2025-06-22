3d039a60f7b30ca7162c29d8d179c748e0228261
Adaśko <ak@gmail.com>
2025-06-22 20:38:14 +0200

    ArticleSeeder.php - zwiększenie ilości testowych artykułów - uzyskanie paginacji

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -12,6 +12,6 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Article::factory()->count(10)->create();
+        \App\Models\Article::factory()->count(25)->create();
     }
 }
