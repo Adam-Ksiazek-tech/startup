45d87b9b9c02ddf574df1b3ea10d6339ecf13912
Adaśko <ak@gmail.com>
2025-06-23 11:37:45 +0200

    Sancturm - token - User.php: trait HasApiTokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
 {
